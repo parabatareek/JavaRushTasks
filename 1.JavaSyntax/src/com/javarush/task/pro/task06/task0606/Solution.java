e66bfb57a45aa5a76457db73ce3991b0651e21fa
parabatareek <gavrosh07@gmail.com>
2022-11-15 21:34:36 +0300

    add: Задачи лекции Параметры функции в Java

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    //напишите тут ваш код
+    public static void universalMethod(String data){
+
+    }
+
+    public static void universalMethod(int data){
+
+    }
+
+    public static void universalMethod(char data){
+
+    }
+
+    public static void universalMethod(long data){
+
+    }
+
+    public static void universalMethod(float data){
+
+    }
+
+    public static void universalMethod(double data){
+
+    }
+
+    public static void universalMethod(String[] data){
+
+    }
+
+    public static void universalMethod(int[] data){
+
+    }
+
+    public static void universalMethod(float[] data){
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0618/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task06.task0618;
+
+/* 
+Злобный призрак
+*/
+
+public class Solution {
+    public static String material= "материальный";
+    public static String immaterial= "нематериальный";
+
+    public static void main(String[] args) {
+        System.out.println("Из болота вылез нематериальный монстр...");
+
+        //напишите тут ваш код
+        switchSword(immaterial);
+    }
+
+    //напишите тут ваш код
+    public static void switchSword(String mode){
+        System.out.println("Меч переключен в " + mode + " режим.");
+    }
+
+}
