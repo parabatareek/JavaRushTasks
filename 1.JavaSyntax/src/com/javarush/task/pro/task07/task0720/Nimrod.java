c0e43077a90e8c3e394704936446efc38e7aa155
parabatareek <gavrosh07@gmail.com>
2022-11-16 17:20:33 +0300

    add: Задачи лекции Объектно-Ориентированное Программирование

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0720/Nimrod.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task07.task0720;
+
+public class Nimrod {
+    public static int laser = 100_000,
+            rocket = 10_000,
+            angelicaKiss;
+    public int health = 1_000_000;
+
+    public void defend(int kick){
+        health -= applyShield(kick);
+    }
+
+    public byte applyShield(int kick){
+        return (byte) kick;
+    }
+
+    public int attack(){
+        return rocket;
+    }
+}
