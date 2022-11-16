ec512ed04acb4c1fac84d557fa30ad7cb686bb73
parabatareek <gavrosh07@gmail.com>
2022-11-16 15:39:34 +0300

    add: Задачи лекции Объекты в Java

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+
+        String s1 = new String();
+        String s2 = new String();
+        String s3 = new String();
+        String s4 = new String();
+        String s5 = new String();
+        String s6 = new String();
+        String s7 = new String();
+        String s8 = new String();
+        String s9 = new String();
+        String s10 = new String();
+
+        int[] i1 = new int[1];
+        int[] i2 = new int[2];
+        int[] i3 = new int[3];
+        int[] i4 = new int[4];
+        int[] i5 = new int[5];
+
+        Scanner scan1 = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+    }
+}
