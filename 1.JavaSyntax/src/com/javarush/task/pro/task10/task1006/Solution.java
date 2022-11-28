6c3e4256ad642f9eae9b3d437c6a955e2859d726
parabatareek <gavrosh07@gmail.com>
2022-11-28 22:57:57 +0300

    add: Задачи лекции Инициализация

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task10.task1006;
+
+/* 
+Кто тут наследник?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        new HybridCar();
+        new ElectricCar();
+        new GasCar();
+    }
+}
