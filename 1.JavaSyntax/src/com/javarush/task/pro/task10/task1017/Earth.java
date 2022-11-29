8ba336ad25a3855c1c7435c53331a654d87635b8
parabatareek <gavrosh07@gmail.com>
2022-11-29 23:19:32 +0300

    add: Задачи лекции Лекция

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        new SouthAmerica(10);
+        new NorthAmerica(11);
+        new Eurasia(12);
+        new Australia(13);
+        new Antarctica(14);
+        new Africa(15);
+    }
+}
