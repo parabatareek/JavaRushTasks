1b8987eaffb6580dce52fa377e1e4fa4471fdabe
parabatareek <gavrosh07@gmail.com>
2022-11-30 12:28:13 +0300

    add: Задачи лекции Загрузка классов

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1129/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task11.task1129;
+
+/* 
+Био-Нано-Боты
+*/
+
+public class Solution {
+
+    public static Human sysadmin = new Human();
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        //Class[] playlist = new Class[]{AlternativeMetal.class};
+        sysadmin.load(new Class[]{AlternativeMetal.class, BlackMetal.class, DeathMetal.class, DoomMetal.class, FolkMetal.class, GlamMetal.class, HeavyMetal.class, GothicMetal.class, IndustrialMetal.class, Metalcore.class, PowerMetal.class, ProgressiveMetal.class, SpeedMetal.class, ThrashMetal.class});
+    }
+}
