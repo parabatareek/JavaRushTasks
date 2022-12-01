2fd284855d80d53ff4ae9f5e0d6f2e21f96773e8
parabatareek <gavrosh07@gmail.com>
2022-12-01 07:13:50 +0300

    add: Задачи лекции Статические переменные

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1130/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1130/Solution.java
@@ -6,12 +6,12 @@ package com.javarush.task.pro.task11.task1130;
 
 public class Solution {
 
-    public final String[] lyrics = {"Du...", "Du hast...", "Du hast ultra metal"};
+    public final static String[] lyrics = {"Du...", "Du hast...", "Du hast ultra metal"};
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        //Solution solution = new Solution();
 
         UltraMetal ultraMetal = new UltraMetal();
-        ultraMetal.playUltraMetal(solution.lyrics[0], solution.lyrics[1], solution.lyrics[2]);
+        ultraMetal.playUltraMetal(lyrics[0], lyrics[1], lyrics[2]);
     }
 }
