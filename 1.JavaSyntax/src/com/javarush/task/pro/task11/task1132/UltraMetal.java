6b8e65ac81a9ac87c4b8b4a16f77753c4671db3b
parabatareek <gavrosh07@gmail.com>
2022-12-05 08:54:10 +0300

    add: Задачи лекции Статические классы

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1132/UltraMetal.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task11.task1132;
+
+public class UltraMetal {
+    public static void play() {
+        for (RoboZombie roboZombie : Solution.roboZombies) {
+
+            roboZombie.uninstallBugs();
+        }
+    }
+}
