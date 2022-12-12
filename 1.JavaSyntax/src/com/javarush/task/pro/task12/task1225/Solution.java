937e28871991b0f61748faecdc0e6dffdd7322bc
parabatareek <gavrosh07@gmail.com>
2022-12-12 20:38:17 +0300

    add: Задачи лекции Коллекции в Java: ArrayList

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1225/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task12.task1225;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/* 
+Аккаунт ИнстаМатрицы
+*/
+
+public class Solution {
+
+    public static ArrayList<String> accountsList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        convertAccounts();
+
+        registerAccount("Амиго");
+        registerAccount("Диего");
+
+        for (String account:
+             accountsList) {
+            System.out.println(account);
+        }
+    }
+
+    public static void convertAccounts() {
+        //напишите тут ваш код
+        accountsList.addAll(0, Arrays.asList(Accounts.getAccounts()));
+    }
+
+    public static void registerAccount(String username) {
+        //напишите тут ваш код
+        if (!accountsList.contains(username)) accountsList.add(username);
+    }
+}
