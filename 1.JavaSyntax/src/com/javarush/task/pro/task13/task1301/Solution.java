6f677d3b35b201bef1199d63ac8930e3d6830150
parabatareek <gavrosh07@gmail.com>
2022-12-14 16:09:23 +0300

    add: Задачи лекции Коллекция HashSet

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1301/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task13.task1301;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+/* 
+Оформляем возврат
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] array = {"Через", "три", "года", "я", "буду", "Senior", "Java", "Developer"};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("___________________________________");
+
+        HashSet<String> hashSet = arrayToHashSet(array);
+        for(String s : hashSet) {
+            System.out.println(s);
+        }
+    }
+
+    public static HashSet<String> arrayToHashSet(String[] strings) {
+        //напишите тут ваш код
+        HashSet dataHash = new HashSet<String>();
+        for (String data:
+             strings) {
+            dataHash.add(data);
+        }
+        return dataHash;
+    }
+}
