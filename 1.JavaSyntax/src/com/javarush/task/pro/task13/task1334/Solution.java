7696f691bfbbafbee5af35eee39f83c56a81ee5e
parabatareek <gavrosh07@gmail.com>
2022-12-18 09:48:03 +0300

    add: Задачи лекции Вспомогательный класс Collections

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1334/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task13.task1334;
+
+/* 
+Антипирамида
+*/
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+    static Map<Voter, Map<String, String>> corporatePyramid = new HashMap<>();
+
+    public static void main(String[] args) {
+        setRewards();
+        printMap(corporatePyramid);
+    }
+
+    public static void setRewards() {
+        //напишите тут ваш код
+        Rewards rewards = new Rewards();
+        for (Voter voter:
+             Voter.generateVoters()) {
+            if (voter.getPersonalQuality().equals("Good")){
+                corporatePyramid.put(voter, rewards.getGoodRewardsMap());
+            }
+            if(voter.getPersonalQuality().equals("Holy")){
+                corporatePyramid.put(voter, rewards.getHolyRewardsMap());
+            }
+        }
+    }
+
+    public static void printMap(Map<?, ?> map) {
+        map.forEach((key, value) -> System.out.println(key + " : " + value));
+    }
+}
