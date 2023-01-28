bfce1c99685cb60d51cde62dedce1cf12cd8b033
parabatareek <gavrosh07@gmail.com>
2023-01-28 11:47:34 +0300

    add: Задачи лекции Работа с файлами в Java

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1516/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task15.task1516;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Файл или директория
+*/
+
+public class Solution {
+    private static final String THIS_IS_FILE = " - это файл";
+    private static final String THIS_IS_DIR = " - это директория";
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String str = scanner.nextLine();
+            if (str.isEmpty()) break;
+                Path path = Path.of(str);
+
+            if (Files.isDirectory(path)) System.out.println(str + THIS_IS_DIR);
+            else if (Files.isRegularFile(path)) System.out.println(str + THIS_IS_FILE);
+            else break;
+
+        }
+    }
+}
+
