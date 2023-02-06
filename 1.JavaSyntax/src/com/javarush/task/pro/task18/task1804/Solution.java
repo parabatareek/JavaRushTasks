603e05b449096b0ee7b91c6971330339f4c2ded2
parabatareek <gavrosh07@gmail.com>
2023-02-06 14:22:11 +0300

    add: Задачи лекции Предыстория появления Лямбда-выражений

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1804/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task18.task1804;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+/* 
+Знакомство с лямбда-выражением
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var numbers = new ArrayList<Integer>();
+
+        Collections.addAll(numbers, 123, -6, 12, 0, 44, 5678, -350);
+
+        sortNumbers(numbers);
+
+        for (Integer number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+    public static void sortNumbers(ArrayList<Integer> numbers) {
+        Collections.sort(numbers, (obj1, obj2) -> obj1.intValue() - obj2.intValue());
+//        Comparator<Integer> comparator = new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer i1, Integer i2) {
+//                return i1 - i2;
+//            }
+//        };
+//        Collections.sort(numbers, comparator);
+    }
+}
