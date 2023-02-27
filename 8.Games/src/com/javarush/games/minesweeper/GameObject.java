4f76e67b8e7db675c2bcea630ff3f86f3df80739
parabatareek <gavrosh07@gmail.com>
2023-02-27 17:13:28 +0300

    add: Задачи 1-8 игры Сапер

---
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -0,0 +1,13 @@
+package com.javarush.games.minesweeper;
+
+public class GameObject {
+    public int x, y;
+    public boolean isMine, isOpen, isFlag;
+    public int countMineNeighbors;
+
+    public GameObject(int x, int y, boolean isMine) {
+        this.x = x;
+        this.y = y;
+        this.isMine = isMine;
+    }
+}
