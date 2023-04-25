84acd71927b2a61c588dad2971964cfedd2056b6
parabatareek <markov.a.n@outlook.com>
2023-04-25 08:30:06 +0300

    add: Задачи лекции Задачи на перегрузку методов

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 0;
+        print(number);
+        print((Integer)number);
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int number){}
+    public static void print(Integer number){}
+
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(String string) {}
+    public static void print(Integer num) {}
+    public static void print(int num) {}
+    public static void print(Double num) {}
+    public static void print(Float num) {}
+}
