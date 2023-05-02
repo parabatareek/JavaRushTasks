388434022e73ade716b787b2427aed39e28c2c39
parabatareek <markov.a.n@outlook.com>
2023-05-02 23:32:20 +0300

    add: Задачи лекции Бонусное задание | 6 лекция | 4 уровень

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1408/MoldovanHen.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task14.task1408;
+
+public class MoldovanHen extends Hen {
+    @Override
+    int getCountOfEggsPerMonth() {
+        return 4;
+    }
+
+    String getDescription() {
+        return super.getDescription() + " Моя страна - "
+                + Country.MOLDOVA
+                + ". Я несу "
+                + getCountOfEggsPerMonth()
+                + " яиц в месяц.";
+    }
+}
