9338492776057d36e9f5b5854016649808d6359f
parabatareek <markov.a.n@outlook.com>
2023-06-12 10:18:15 +0300

    add: Задачи лекции Задачи на Reader/Writer

--- 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
@@ -1,79 +1,103 @@
 package com.javarush.task.task18.task1828;
 
-import java.io.*;
-import java.util.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
-/* 
+/*
 Прайсы 2
 */
 
 public class Solution {
-    private static HashMap<Integer, Product> products = new HashMap<>();
-    private static File dataFile;
+    public static class Product {
+        int id;
+        String name;
+        String price;
+        String quantity;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        dataFile = new File(scanner.nextLine());
-
-        dataInit();
-
-        switch (args[0]) {
-            case "-u":
-                update(new Product(Integer.valueOf(args[1]),
-                        args[4],
-                        args[2],
-                        args[3]));
-                break;
-            case "-d":
-                delete(Integer.valueOf(args[0]));
-                break;
-        }
-    }
-
-    private static void dataInit() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(dataFile))){
-            while (reader.ready()){
-                Product product = new Product(reader.readLine());
-                products.put(product.id, product);
-            }
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void delete(Integer id) {
-    }
-
-    private static void update(Product product) {
-        products.
-    }
-
-    private static class Product {
-        private int id;
-        private String productName, price, quantity;
-
-        public Product(int id, String quantity, String productName, String price) {
+        public Product(int id, String name, String price, String quantity) {
             this.id = id;
-            this.quantity = quantity;
-            this.productName = productName;
+            this.name = name;
             this.price = price;
-        }
-
-        public Product(String data) {
-            this.id = Integer.parseInt(data.substring(0,8).trim());
-            this.productName = data.substring(8, 38).trim();
-            this.price = data.substring(38, 46).trim();
-            this.quantity = data.substring(46, 50).trim();
+            this.quantity = quantity;
         }
 
         @Override
         public String toString() {
-            return String.format("%-8d%-30s%-8s%-4s", id, productName, price, quantity);
+            return String.format("%-8d%-30s%-8s%-4s", id, name, price, quantity);
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+
+        List<Product> products = new ArrayList<>();
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+            while (fileReader.ready()) {
+                Product product = getProduct(fileReader.readLine());
+                products.add(product);
+            }
+        }
+
+        switch (args[0]) {
+            case "-u": {
+                int id = Integer.parseInt(args[1]);
+                String name = "";
+                for (int i = 2; i < args.length - 2; i++) {
+                    name += args[i] + " ";
+                }
+                if (name.length() > 30) {
+                    name = name.substring(0, 30);
+                }
+                String price = args[args.length - 2];
+                if (price.length() > 8) {
+                    price = price.substring(0, 8);
+                }
+                String quantity = args[args.length - 1];
+                if (quantity.length() > 4) {
+                    quantity = quantity.substring(0, 4);
+                }
+                Product productToUpdate = null;
+                for (Product product : products) {
+                    if (product.id == id) productToUpdate = product;
+                }
+                if (productToUpdate != null) {
+                    productToUpdate.name = name;
+                    productToUpdate.price = price;
+                    productToUpdate.quantity = quantity;
+                }
+                break;
+            }
+            case "-d": {
+                int id = Integer.parseInt(args[1]);
+                Product productToDelete = null;
+                for (Product product : products) {
+                    if (product.id == id) productToDelete = product;
+                }
+                if (productToDelete != null) products.remove(productToDelete);
+                break;
+            }
+        }
+
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+            for (Product product : products) {
+                fileWriter.write(product.toString());
+                fileWriter.write("\n");
+            }
+        }
+    }
+
+    public static Product getProduct(String string) {
+        String id = string.substring(0, 8).trim();
+        String name = string.substring(8, 38).trim();
+        String price = string.substring(38, 46).trim();
+        String quantity = string.substring(46, 50).trim();
+        return new Product(Integer.parseInt(id), name, price, quantity);
     }
 }
